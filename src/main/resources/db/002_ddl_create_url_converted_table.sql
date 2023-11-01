8dbbf9684479f26f20677ee18af4cb5c22c67d45
Dilshod Musakhanov <musakhanov@yahoo.com>
2023-11-01 05:07:05 +0400

    refactoring

--- src/main/resources/db/002_ddl_create_url_converted_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE url_converted (
-    id SERIAL PRIMARY KEY NOT NULL,
-    url TEXT NOT NULL UNIQUE,
-    url_shortcut VARCHAR(10) NOT NULL UNIQUE,
-    visits INT NOT NULL,
-    site_id INT,
-    CONSTRAINT fk_site_id FOREIGN KEY (site_id) REFERENCES site(id)
-);
